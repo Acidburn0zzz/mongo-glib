74dd67c320dd7f813ee2dd3646586acd910a028e
Christian Hergert <chris@dronelabs.com>
2012-06-29 13:56:38 -0700

    [examples] Cleanup mongo-glib vs pymongo bson performance.

--- examples/bson_tests.py
+++ examples/bson_tests.py
@@ -24,27 +24,24 @@ def main():
 
     n_runs = 30
 
-    if 'glib' in sys.argv:
-        total = 0
-        for i in range(n_runs):
-            t = runTimed(test1)
-            total += t
-            print 'Mongo.Bson', t
-        print '====='
-        print 'Average:', total / float(n_runs)
-        sys.exit(0)
+    glibtotal = 0
+    bsontotal = 0
 
-    if 'bson' in sys.argv:
-        total = 0
-        for i in range(n_runs):
-            t = runTimed(test2)
-            total += t
-            print '      bson', t
-        print '====='
-        print 'Average:', total / float(n_runs)
-        sys.exit(0)
+    for i in range(n_runs):
+        t1 = runTimed(test1)
+        glibtotal += t1
+        print 'Mongo.Bson:', t1
+        t2 = runTimed(test2)
+        bsontotal += t2
+        print '      bson:', t2
+        print '   Speedup:', t2 / t1
+        print
 
-    print 'Specify glib or bson'
+    print '====='
+    print 'GLib Average:', glibtotal / float(n_runs)
+    print 'BSON Average:', bsontotal / float(n_runs)
+
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
