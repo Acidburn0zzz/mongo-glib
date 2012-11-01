b8cec1481a871bcf92e10cdf4d253dd5f4fd7b51
Christian Hergert <christian@hergert.me>
2012-10-31 19:29:09 -0700

    Some autotools updating of the build system.

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-test -f "configure.ac" || {
-	echo "You must run this script in the top-level mongo-glib directory"
-	exit 1
-}
-
-autoreconf -v --install || exit $?
-
-test -n "$NOCONFIGURE" ||
-(
-	"$srcdir/configure" "$@" &&
-	echo "Now type 'make' to compile mongo-glib"
-)
+gnome-autogen.sh $@
