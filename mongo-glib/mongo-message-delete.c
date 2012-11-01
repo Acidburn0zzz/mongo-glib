57c7cc9025532df9453ca05299322790b6713c97
Christian Hergert <chris@dronelabs.com>
2012-10-31 17:09:56 -0700

    Implement MongoMessageBsonClass->load_from_data().

--- mongo-glib/mongo-message-delete.c
+++ mongo-glib/mongo-message-delete.c
@@ -18,38 +18,194 @@
 
 #include <glib/gi18n.h>
 
+#include "mongo-debug.h"
 #include "mongo-message-delete.h"
 
 G_DEFINE_TYPE(MongoMessageDelete, mongo_message_delete, MONGO_TYPE_MESSAGE)
 
 struct _MongoMessageDeletePrivate
 {
-   gpointer dummy;
+   gchar            *collection;
+   MongoDeleteFlags  flags;
+   MongoBson        *selector;
 };
 
 enum
 {
    PROP_0,
+   PROP_COLLECTION,
+   PROP_FLAGS,
+   PROP_SELECTOR,
    LAST_PROP
 };
 
-//static GParamSpec *gParamSpecs[LAST_PROP];
+static GParamSpec *gParamSpecs[LAST_PROP];
+
+const gchar *
+mongo_message_delete_get_collection (MongoMessageDelete *delete)
+{
+   g_return_val_if_fail(MONGO_IS_MESSAGE_DELETE(delete), NULL);
+   return delete->priv->collection;
+}
+
+void
+mongo_message_delete_set_collection (MongoMessageDelete *delete,
+                                     const gchar        *collection)
+{
+   MongoMessageDeletePrivate *priv;
+
+   g_return_if_fail(MONGO_IS_MESSAGE_DELETE(delete));
+
+   priv = delete->priv;
+
+   g_free(priv->collection);
+   priv->collection = g_strdup(collection);
+   g_object_notify_by_pspec(G_OBJECT(delete), gParamSpecs[PROP_COLLECTION]);
+}
+
+MongoBson *
+mongo_message_delete_get_selector (MongoMessageDelete *delete)
+{
+   g_return_val_if_fail(MONGO_IS_MESSAGE_DELETE(delete), NULL);
+   return delete->priv->selector;
+}
+
+void
+mongo_message_delete_set_selector (MongoMessageDelete *delete,
+                                   MongoBson          *selector)
+{
+   MongoMessageDeletePrivate *priv;
+
+   g_return_if_fail(MONGO_IS_MESSAGE_DELETE(delete));
+
+   priv = delete->priv;
+
+   if (priv->selector) {
+      mongo_bson_unref(priv->selector);
+      priv->selector = NULL;
+   }
+
+   if (selector) {
+      priv->selector = mongo_bson_ref(selector);
+   }
+
+   g_object_notify_by_pspec(G_OBJECT(delete), gParamSpecs[PROP_SELECTOR]);
+}
+
+MongoDeleteFlags
+mongo_message_delete_get_flags (MongoMessageDelete *delete)
+{
+   g_return_val_if_fail(MONGO_IS_MESSAGE_DELETE(delete), 0);
+   return delete->priv->flags;
+}
+
+void
+mongo_message_delete_set_flags (MongoMessageDelete *delete,
+                                MongoDeleteFlags    flags)
+{
+   g_return_if_fail(MONGO_IS_MESSAGE_DELETE(delete));
+   delete->priv->flags = flags;
+   g_object_notify_by_pspec(G_OBJECT(delete), gParamSpecs[PROP_FLAGS]);
+}
+
+static gboolean
+mongo_message_delete_load_from_data (MongoMessage *message,
+                                     const guint8 *data,
+                                     gsize         length)
+{
+   MongoMessageDeletePrivate *priv;
+   MongoMessageDelete *delete = (MongoMessageDelete *)message;
+   const gchar *name;
+   MongoBson *bson;
+   guint32 len;
+
+   g_assert(MONGO_IS_MESSAGE_DELETE(delete));
+   g_assert(data);
+   g_assert(length);
+
+   priv = delete->priv;
+
+   if (length >= 4) {
+      /* Skip, first 4 bytes are reserved. */
+      data += 4;
+      length -= 4;
+      if (length > 1) {
+         for (name = (gchar*)data; *data && length; length--, data++) {}
+         if (length) {
+            data++;
+            length--;
+            if (g_utf8_validate(name, ((gchar*)data) - name, NULL)) {
+               mongo_message_delete_set_collection(delete, name);
+               if (length >= 4) {
+                  memcpy(&priv->flags, data, sizeof priv->flags);
+                  priv->flags = GUINT32_FROM_LE(priv->flags);
+                  data += 4;
+                  length -= 4;
+                  if (length >= 4) {
+                     memcpy(&len, data, sizeof len);
+                     len = GUINT32_FROM_LE(len);
+                     if (len == length) {
+                        bson = mongo_bson_new_from_data(data, len);
+                        mongo_message_delete_set_selector(delete, bson);
+                        mongo_bson_unref(bson);
+                        return TRUE;
+                     }
+                  }
+               }
+            }
+         }
+      }
+   }
+
+   return FALSE;
+}
+
+static guint8 *
+mongo_message_delete_save_to_data (MongoMessage *message,
+                                   gsize        *length)
+{
+   return NULL;
+}
 
 static void
 mongo_message_delete_finalize (GObject *object)
 {
+   MongoMessageDeletePrivate *priv;
+
+   ENTRY;
+
+   priv = MONGO_MESSAGE_DELETE(object)->priv;
+
+   if (priv->selector) {
+      mongo_bson_unref(priv->selector);
+      priv->selector = NULL;
+   }
+
+   g_free(priv->collection);
+
    G_OBJECT_CLASS(mongo_message_delete_parent_class)->finalize(object);
+
+   EXIT;
 }
 
 static void
 mongo_message_delete_get_property (GObject    *object,
-                           guint       prop_id,
-                           GValue     *value,
-                           GParamSpec *pspec)
+                                   guint       prop_id,
+                                   GValue     *value,
+                                   GParamSpec *pspec)
 {
-   //MongoMessageDelete *delete = MONGO_MESSAGE_DELETE(object);
+   MongoMessageDelete *delete = MONGO_MESSAGE_DELETE(object);
 
    switch (prop_id) {
+   case PROP_COLLECTION:
+      g_value_set_string(value, mongo_message_delete_get_collection(delete));
+      break;
+   case PROP_FLAGS:
+      g_value_set_flags(value, mongo_message_delete_get_flags(delete));
+      break;
+   case PROP_SELECTOR:
+      g_value_set_boxed(value, mongo_message_delete_get_selector(delete));
+      break;
    default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
    }
@@ -57,13 +213,22 @@ mongo_message_delete_get_property (GObject    *object,
 
 static void
 mongo_message_delete_set_property (GObject      *object,
-                           guint         prop_id,
-                           const GValue *value,
-                           GParamSpec   *pspec)
+                                   guint         prop_id,
+                                   const GValue *value,
+                                   GParamSpec   *pspec)
 {
-   //MongoMessageDelete *delete = MONGO_MESSAGE_DELETE(object);
+   MongoMessageDelete *delete = MONGO_MESSAGE_DELETE(object);
 
    switch (prop_id) {
+   case PROP_COLLECTION:
+      mongo_message_delete_set_collection(delete, g_value_get_string(value));
+      break;
+   case PROP_FLAGS:
+      mongo_message_delete_set_flags(delete, g_value_get_flags(value));
+      break;
+   case PROP_SELECTOR:
+      mongo_message_delete_set_selector(delete, g_value_get_boxed(value));
+      break;
    default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
    }
@@ -73,12 +238,17 @@ static void
 mongo_message_delete_class_init (MongoMessageDeleteClass *klass)
 {
    GObjectClass *object_class;
+   MongoMessageClass *message_class;
 
    object_class = G_OBJECT_CLASS(klass);
    object_class->finalize = mongo_message_delete_finalize;
    object_class->get_property = mongo_message_delete_get_property;
    object_class->set_property = mongo_message_delete_set_property;
    g_type_class_add_private(object_class, sizeof(MongoMessageDeletePrivate));
+
+   message_class = MONGO_MESSAGE_CLASS(klass);
+   message_class->load_from_data = mongo_message_delete_load_from_data;
+   message_class->save_to_data = mongo_message_delete_save_to_data;
 }
 
 static void
